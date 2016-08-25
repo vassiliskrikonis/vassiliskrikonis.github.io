a33da9d43f5d53a4453747857b336a8c633fc64f
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2016-08-25 15:37:49 +0300

    simpler way to have different underline color
    I can’t believe it was that simple. I feel so dumb now. I was
    duplicating the content before to have it beneath the original content
    with different colour. But oh well, I’ve gained a bit of know-how now.
    It could be useful in the future I guess.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var $ = jQuery;
 
-$(function() {
-	var originals = $('.sectionz.container section');
-	var dups = originals.clone();
-	dups.find('a').each(function() {
-		$(this).replaceWith($('<span class="link">' + this.innerHTML + '</span>'));
-	});
-	$('.sectionz.container').append(dups);
-	dups.wrapAll('<div class="dups"></div>');
-});
+// $(function() {
+// 	var originals = $('.sectionz.container section');
+// 	var dups = originals.clone();
+// 	dups.find('a').each(function() {
+// 		$(this).replaceWith($('<span class="link">' + this.innerHTML + '</span>'));
+// 	});
+// 	$('.sectionz.container').append(dups);
+// 	dups.wrapAll('<div class="dups"></div>');
+// });
 
 
 function fixSize() {
