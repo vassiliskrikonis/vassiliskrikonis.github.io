ef3c91037afa249922e535531825f0f4fdad6e0b
vassiliskrikonis <vasiliskrikonis@gmail.com>
2022-07-18 19:14:12 +0000

    Create class for element with attached body

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import Matter from 'matter-js';
 import Hammer from 'hammerjs';
+import DOMBody from './DOMBody';
 
 // blast dom into .blast spans
 var blasted = $('p').blast({ delimiter: "word" }).toArray();
@@ -8,8 +9,8 @@ var blasted = $('p').blast({ delimiter: "word" }).toArray();
 var engine = Matter.Engine.create();
 
 // create bodies for each element
-var bodies = blastedToBodies(blasted);
-Matter.World.add(engine.world, bodies);
+var domBodies = blastedToDomBodies(blasted);
+Matter.World.add(engine.world, domBodies.map(it => it.body));
 
 // create mouseBody for "touching away" and colliding with other bodies
 var mouseBody = Matter.Bodies.circle(0, 0, 30, {isStatic: true});
@@ -26,7 +27,7 @@ onTouchMove((e) => {
 //   transformElementsFrom(bodies);
 // });
 function step() {
-  transformElementsFrom(bodies);
+  domBodies.forEach(body => body.updatePosition());
   requestAnimationFrame(step);
 }
 step();
@@ -36,38 +37,12 @@ engine.world.gravity.scale = 0.000000000001;
 Matter.Engine.run(engine);
 
 
-function blastedToBodies(elements) {
-  return elements.map(function(elem) {
-    var $elem = $(elem);
-    var left = $elem.offset().left;
-    var top = $elem.offset().top;
-    var w = $elem.width();
-    var h = $elem.height();
-    var x = left + w/2; // Matter.js uses center-origin shapes
-    var y = top + h/2;
-    var body = Matter.Bodies.rectangle(x, y, w, h, {frictionAir: 0.1});
-    body.origX = x; // save initial position
-    body.origY = y;
-    body.ref = elem; // keep a reference to the DOM element to transform it
-    return body;
-  });
+function blastedToDomBodies(elements) {
+  return elements.map((elem) => DOMBody.fromElement(elem));
 }
 
 function onTouchMove(f) {
   var hammertime = this.__hammertime || (this.__hammertime = new Hammer(document.body));
   hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
   hammertime.on('pan', f);
-}
-
-function transformElementsFrom(bodies) {
-  bodies.forEach(function(body) {
-    if(body.isSleeping)
-      return;
-
-    var elem = body.ref;
-    var dx = body.position.x - body.origX;
-    var dy = body.position.y - body.origY;
-    
-    elem.style.transform = `translate(${dx}px, ${dy}px)`;
-  });
 }
